ba3648739eae30fd48440068bab4407430d93dfa
GuilleCalM <guilleacalderon@hotmail.com>
2024-10-23 18:16:34 -0500

    first commit

---
+++ src/main/java/com/cibertec/edu/api_reservacion/DswiiEfCalderonMontesGuillermoAnthoniApiReservacionApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec.edu.api_reservacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiEfCalderonMontesGuillermoAnthoniApiReservacionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DswiiEfCalderonMontesGuillermoAnthoniApiReservacionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cibertec/edu/api_reservacion/DswiiEfCalderonMontesGuillermoAnthoniApiReservacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec.edu.api_reservacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiiEfCalderonMontesGuillermoAnthoniApiReservacionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
